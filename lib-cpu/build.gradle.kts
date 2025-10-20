b4bb7db93b3fe49f5857309a060d6fa6f521e0ce
ookk1234 <492436094@qq.com>
2025-10-20 11:32:30 +0800

    Update build.gradle.kts
    移除sources

--- lib-cpu/build.gradle.kts
+++ lib-cpu/build.gradle.kts
@@ -50,6 +50,11 @@ afterEvaluate {
                 artifactId = "cpu"
                 version = "1.0.0"
             }
+
+            withType<MavenPublication> {
+                // 移除 Gradle 自动添加的 sources.jar artifact
+                artifacts.removeIf { it.classifier == "sources" }
+            }
         }
     }
 }
